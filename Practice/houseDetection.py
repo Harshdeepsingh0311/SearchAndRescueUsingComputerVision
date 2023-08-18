405a06966c7e654932c02e2339afc625ee94484c
harshdeep <harshdeepsinghbakshi2005@gmail.com>
2023-08-19 00:11:00 +0530

    Shape And Color Detection

--- Practice/houseDetection.py
+++ Practice/houseDetection.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('Shapes/triangle.png') # Reading the image
+img = cv2.imread('Shapes/shapes.png') # Reading the image
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Converting it to grayscale
 _, thrash = cv2.threshold(imgGray, 240, 255, cv2.THRESH_BINARY) 
 contours, _ = cv2.findContours(thrash, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) # Searching the contours of the shapes
@@ -18,7 +18,7 @@ for contour in contours:
     y = approx.ravel()[1] - 5
     if len(approx) == 3:
         # It's a triangle
-        cv2.putText(img, "House", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+        cv2.putText(img, "House", (x, y), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255))
     
 
 # Showing the img after detection
